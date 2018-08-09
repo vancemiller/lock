a3fd70bfe2d93b31c2acbd8a169db90a06f60dd0
Vance Miller <vance@cs.utexas.edu>
2018-08-09 10:18:52 -0500

    error messages

--- include/lock.hpp
+++ include/lock.hpp
@@ -12,14 +12,16 @@ class Mutex {
   public:
     Mutex(void) {
       pthread_mutexattr_t attr;
-      if (pthread_mutexattr_init(&attr) != 0)
-        throw std::runtime_error("Failed to initialize pthread mutex attributes");
-      if (pthread_mutexattr_setpshared(&attr, PTHREAD_PROCESS_SHARED) != 0)
-        throw std::runtime_error("Failed to set pshared pthread mutex attributes");
-      if (pthread_mutex_init(&mutex, &attr) != 0)
-        throw std::runtime_error("Failed to initialize pthread mutex");
-      if (pthread_mutexattr_destroy(&attr) != 0)
-        throw std::runtime_error("Failed to destroy pthread mutex attributes");
+      int err;
+      if ((err = pthread_mutexattr_init(&attr)) != 0)
+        throw std::system_error(err, std::generic_category(), "pthread_mutex_attr_init failed");
+      if ((err = pthread_mutexattr_setpshared(&attr, PTHREAD_PROCESS_SHARED)) != 0)
+        throw std::system_error(err, std::generic_category(),
+            "pthread_mutexattr_setpshared failed");
+      if ((err = pthread_mutex_init(&mutex, &attr)) != 0)
+        throw std::system_error(err, std::generic_category(), "pthread_mutex_init failed");
+      if ((err = pthread_mutexattr_destroy(&attr)) != 0)
+        throw std::system_error(err, std::generic_category(), "pthread_mutexattr_destroy failed");
     }
 
     ~Mutex(void) {
@@ -28,13 +30,15 @@ class Mutex {
     }
 
     void lock(void) {
-      if (pthread_mutex_lock(&mutex) != 0)
-        throw std::runtime_error("Failed to lock pthread mutex");
+      int err = pthread_mutex_lock(&mutex);
+      if (err)
+        throw std::system_error(err, std::generic_category(), "pthread_mutex_lock failed");
     }
 
     void unlock(void) {
-      if (pthread_mutex_unlock(&mutex) != 0)
-        throw std::runtime_error("Failed to unlock pthread mutex");
+      int err = pthread_mutex_unlock(&mutex);
+      if (err)
+        throw std::system_error(err, std::generic_category(), "pthread_mutex_unlock failed");
     }
 };
 
@@ -44,14 +48,15 @@ class Condition {
   public:
     Condition(void) {
       pthread_condattr_t attr;
-      if (pthread_condattr_init(&attr) != 0)
-        throw std::runtime_error("Failed to initialize pthread cond attributes");
-      if (pthread_condattr_setpshared(&attr, PTHREAD_PROCESS_SHARED) != 0)
-        throw std::runtime_error("Failed to set pshared pthread cond attribute");
-      if (pthread_cond_init(&condition, &attr) != 0)
-        throw std::runtime_error("Failed to initialize pthread cond");
-      if (pthread_condattr_destroy(&attr) != 0)
-        throw std::runtime_error("Failed to destroy pthread cond attributes");
+      int err;
+      if ((err = pthread_condattr_init(&attr)) != 0)
+        throw std::system_error(err, std::generic_category(), "pthread_condattr_init failed");
+      if ((err = pthread_condattr_setpshared(&attr, PTHREAD_PROCESS_SHARED)) != 0)
+        throw std::system_error(err, std::generic_category(), "pthread_condattr_setpshared failed");
+      if ((err = pthread_cond_init(&condition, &attr)) != 0)
+        throw std::system_error(err, std::generic_category(), "pthread_cond_init failed");
+      if ((err = pthread_condattr_destroy(&attr)) != 0)
+        throw std::system_error(err, std::generic_category(), "pthread_condattr_destroy failed");
     }
 
     ~Condition(void) {
@@ -61,17 +66,16 @@ class Condition {
 
     // call with locked mutex. Unlocked during wait, locked after wait
     void wait(Mutex& mutex) {
-      int err;
-      if ((err = pthread_cond_wait(&condition, &mutex.mutex)) != 0)
-        throw std::runtime_error("Failed to wait pthread cond: " + std::string(std::strerror(err)));
+      int err = pthread_cond_wait(&condition, &mutex.mutex);
+      if (err)
+        throw std::system_error(err, std::generic_category(), "pthread_cond_wait failed");
     }
 
     void broadcast(void) {
-      if (pthread_cond_broadcast(&condition) != 0)
-        throw std::runtime_error("Failed to broadcast pthread cond");
+      int err = pthread_cond_broadcast(&condition);
+      if (err)
+        throw std::system_error(err, std::generic_category(), "pthread_cond_broadcast failed");
     }
 };
 
-
 #endif
-
